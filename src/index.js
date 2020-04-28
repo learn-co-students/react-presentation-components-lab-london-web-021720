8534ec19cdd0e4ff5dee2bf512408490629e8aa0
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-28 13:02:22 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={HTMLFormControlsCollection.log("Hi")} />
   </div>,
   document.getElementById('root')
 );
